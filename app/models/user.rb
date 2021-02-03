33b69dba37484801deb0e46a4b3eedafe4e13982
Huemac Alfredo Córdova <hmca101292@outlook.com>
2021-02-03 17:43:34 -0600

    AAdd user id and poste id

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,3 +2,10 @@ class User < ActiveRecord::Base
   validates :username, presence: true, uniqueness: true,
             length: { maximum: 20 }
 end
+class User < ActiveRecord::Base
+  validates :username, presence: true, uniqueness: true,
+            length: { maximum: 20 }
+  
+  has_many :posts
+  has_many :comments
+end
